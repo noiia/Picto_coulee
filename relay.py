85c95f15579aef37590e6a1317f36d7982e5410b
Noia <49865944+noiia@users.noreply.github.com>
2022-10-27 01:14:59 +0200

    Create relay.py

---
+++ relay.py
@@ -0,0 +1,29 @@
+import RPi.GPIO as gpio
+import time
+from signal import signal, SIGINT
+from sys import exit
+ 
+def handler(signal_received, frame):
+    # on gère un cleanup propre
+    print('')
+    print('SIGINT or CTRL-C detected. Exiting gracefully')
+    gpio.cleanup()
+    exit(0)
+ 
+def main():
+    # on passe en mode BMC qui veut dire que nous allons utiliser directement
+    # le numero GPIO plutot que la position physique sur la carte
+    gpio.setmode(gpio.BCM)
+ 
+    # defini le port GPIO 4 comme etant une sortie output
+    gpio.setup(4, gpio.OUT)
+ 
+    # Mise a 1 pendant 2 secondes puis 0 pendant 2 seconde
+    while True:
+        print("on")
+        gpio.output(4, gpio.HIGH)
+        time.sleep(2)
+        print("off")
+        gpio.output(4, gpio.LOW)
+        time.sleep(2)
+ 
